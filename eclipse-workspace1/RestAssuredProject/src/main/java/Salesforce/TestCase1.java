14128f9566b6a230aa0147e36730a75cd1ad0c46
Brigutej <brigutej@gmail.com>
2021-03-08 09:30:58 -0500

    Added steps

--- eclipse-workspace1/RestAssuredProject/src/main/java/Salesforce/TestCase1.java
+++ eclipse-workspace1/RestAssuredProject/src/main/java/Salesforce/TestCase1.java
@@ -23,39 +23,51 @@ public class TestCase1 {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-		// Example 1
+		// Login to https://login.salesforce.com
 		driver.get("https://login.salesforce.com");
 
 		driver.findElement(By.id("username")).sendKeys("mercury.bootcamp@testleaf.com");
 		driver.findElement(By.id("password")).sendKeys("Bootcamp$123");
 		driver.findElement(By.id("Login")).click();
-
 		Thread.sleep(5000);
+		
+		//Click on the toggle menu button from the left corner
 		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
 		Thread.sleep(5000);
+		
+		//Click View All
 		driver.findElement(By.xpath("//button[@class='slds-button']")).click();
 		Thread.sleep(3000);
+		
+		//click Sales from App Launcher
 		driver.findElement(By.xpath("//p[text()='Sales']")).click();
 		Thread.sleep(3000);
 
+		//Click on the Opportunity tab
 		WebElement opclick = driver.findElement(By.xpath("//span[text()='Opportunities']"));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click();", opclick);
 		
+		//Click on the New button
 		 driver.findElement(By.xpath("//a[@class='forceActionLink']")).click();
 		 Thread.sleep(5000); 
-			
+		
+		 //Enter Opportunity name as 'Salesforce Automation by Your Name, Get the text and Store it 
 		 WebElement text= driver.findElement(By.xpath("(//input[@class=' input'])[2]"));
-				text.sendKeys("Salesforce Automation by Brinda");
+		 text.sendKeys("Salesforce Automation by Brinda");
 		 String expectedText=text.getAttribute("value");
 		 System.out.println(expectedText);
 		 
+		 //Choose a close date as Today
 		 WebElement datepicker=driver.findElement(By.xpath("//span[text()='Date Picker']"));
-		 JavascriptExecutor dp = (JavascriptExecutor) driver;
-		 dp.executeScript("arguments[0].click();", datepicker);
+		 js.executeScript("arguments[0].click();", datepicker);
 		 driver.findElement(By.xpath("//button[text()='Today']")).click();
 		 driver.findElement(By.xpath("(//a[@class='select'])[1]")).click();
+		 
+		 //Select 'Stage' as Need Analysis
 		 driver.findElement(By.xpath("//a[text()='Needs Analysis']")).click();
+		 
+		 //click Save and VerifyOppurtunity Name"
 		 driver.findElement(By.xpath("(//span[text()='Save'])[2]")).click();
 		 String actualText=driver.findElement(By.xpath("//slot[@slot='primaryField']/lightning-formatted-text")).getText();
 		 
